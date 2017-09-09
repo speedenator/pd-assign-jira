c86ba9f438131323674a29b8c56716dc5378fb83
Erik Selberg <erik@selberg.org>
2017-09-09 16:43:42 -0700

    inc

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/webhook4', methods=['POST'])
-def webhook4():
+@app.route('/webhook5', methods=['POST'])
+def webhook5():
 
     print("Here we go!")
 
